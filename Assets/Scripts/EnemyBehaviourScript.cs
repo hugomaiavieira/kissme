9138397f83fb6b2ff01a60bb221d983e2cd65147
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2014-12-25 13:20:54 -0200

    just move the enemies from right to left. moving in the players direction makes the game too difficult

--- Assets/Scripts/EnemyBehaviourScript.cs
+++ Assets/Scripts/EnemyBehaviourScript.cs
@@ -3,40 +3,18 @@ using System.Collections;
 
 public class EnemyBehaviourScript : MonoBehaviour {
 
-	float MAX_DISTANCE = 1.5f;
-
 	public float speed;
-	GameObject player;
-	Vector3 direction;
-	Vector3 directionNormalized;
-	bool attacking = true;
+	Vector3	direction = new Vector3(-1, 0 , 0);
 	
 	void Start () {
-		player = GameObject.FindWithTag("Player");
-		CalculateDirection();
 	}
 
 	void Update () {
 		Attack();
 	}
 
-	// Attack the player until reaches some distance of him
 	public void Attack() {
-		float distance = Vector3.Distance(player.transform.position, transform.position);
-
-		if (distance > MAX_DISTANCE && attacking) {
-			CalculateDirection();
-		} 
-		else if (distance <= MAX_DISTANCE && attacking) {
-			attacking = false;
-		}
-
 		transform.Translate(speed * direction * Time.deltaTime);
-	}
-	
-	// Get the normalized (unitary) direction vector from the enemy to the player
-	private void CalculateDirection () {
-		direction = (player.transform.position - transform.position).normalized;
 	}
 
 }

bac5a20cda4bf090ef225cb3f98d9cd6cfd9f927
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2015-01-25 18:00:06 -0200

    add old man

---
+++ Assets/Scripts/OldManBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldManBehaviour : MonoBehaviour {
+
+	public float speed;
+
+	Vector3 		direction = new Vector3(-1, 0 , 0);
+	GameController 	gameController;
+
+	// Use this for initialization
+	void Start () {
+		gameController = FindObjectOfType(typeof(GameController)) as GameController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameController.IsInGame())
+			transform.Translate(speed * direction * Time.deltaTime);
+	}
+}

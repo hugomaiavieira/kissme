82053b332e4e6f97bf1162ec287d6c2dc6c67ec7
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2015-01-26 23:51:29 -0200

    make image sequences from final stage

--- Assets/Scripts/AirplaneBehaviour.cs
+++ Assets/Scripts/AirplaneBehaviour.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class AirplaneBehaviour : MonoBehaviour {
 
+	FinalLevelController gameController;
+
+	void Start() {
+		gameController = FindObjectOfType(typeof(FinalLevelController)) as FinalLevelController;
+	}
+
 	public void PlayTakeoffSound() {
 		SoundController.PlaySound(gameSounds.airplane);
+	}
+
+	public void AirplaneLand() {
+		gameController.StartIlustrationSequence();
 	}
 }
---
+++ Assets/Scripts/FinalLevelController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum FinalLevelGameState {
+	WAITGAME,
+	INITIALANIMATION,
+	ILUSTRATIIONSEQUENCE,
+	FINISH
+}
+
+public class FinalLevelController : MonoBehaviour {
+
+	public List<GameObject>		finalImages;
+	public AirplaneBehaviour	airplane;
+
+	Animator       	animator;
+
+	FinalLevelGameState  currentState = FinalLevelGameState.WAITGAME;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		Debug.Log(currentState);
+
+		switch(currentState) {
+			case FinalLevelGameState.WAITGAME: {
+				if(TouchEvent()) {
+					currentState = FinalLevelGameState.INITIALANIMATION;
+					Animator animator = airplane.GetComponent<Animator>();
+					animator.SetTrigger("Takeoff");
+				}
+			}
+			break;
+		}
+	}
+
+	public void StartIlustrationSequence() {
+		currentState = FinalLevelGameState.ILUSTRATIIONSEQUENCE;
+		StartCoroutine(ShowIlustrationSequence());
+	}
+	
+
+	public IEnumerator ShowIlustrationSequence() {
+		Fading fading = GetComponent<Fading>();
+		float interval = 4f;
+
+		for (int i=0; i < finalImages.Count; i++) {
+			fading.BeginFade(1);
+			yield return new WaitForSeconds(fading.fadeSpeed);
+			finalImages[i].SetActive(true);
+			fading.BeginFade(-1);
+			
+			yield return new WaitForSeconds(interval);
+		}
+	}
+	
+	public bool TouchEvent() {
+		return Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space);
+	}
+}
